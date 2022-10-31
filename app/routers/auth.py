3f4bd65b1700803e802e77184df03a48db248ecf
Ali Naseri <an658@ad.cam.ac.uk>
2022-10-31 12:02:30 +0000

    dev-pyapi: token verification is added. In the next step, user authentication must be included for creating a post

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Response, Depends, status
 from sqlalchemy.orm import Session
 from app.schemas import UserLogin
+# Import the password request form instead of using your own data model
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 from app.database import get_db
 from app import models
@@ -10,16 +12,17 @@ from app.routers import oauth2
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: UserLogin,  db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(),  db: Session = Depends(get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    # Using OAuth2Passwrod form, there is no dedicated email field. The email is stored in the username 
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail='Invalid Credintials')
     
     if not verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail='Invalid Credintials') 
         
     # create token 
--- app/routers/oauth2.py
+++ app/routers/oauth2.py
@@ -1,5 +1,12 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from app import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+# define your password endpoint object
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 
 # SECRET_KEY 
 # Algorithm
@@ -14,9 +21,28 @@ def create_access_token(data: dict):
     # make a copy of the passed data
     to_encode = data.copy()
     
-    expire= datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire= datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_crruent_user(token: str = Depends(oauth2_scheme)):
+    credintials_exception = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Could not validate credintials",
+    headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credintials_exception)
