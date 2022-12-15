18eb829eb99b3bb6bbaccb021d3a2f425326b436
Ali Naseri <alinaseri.ut@gmail.com>
2022-12-15 17:21:56 +0000

    The vote table is added and works with API

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, conint
 from typing import Optional
 
 
@@ -50,3 +50,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+    
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1) # you just need a datatype that can provde only 0 and 1
