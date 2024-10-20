21f0cd1a0cca5bcb8e4ed357c5195fbd32b01732
Ali Naseri <alinasericodes@gmail.com>
2024-10-20 14:53:14 -0400

    Handle voting to non-existent posts

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -12,6 +12,15 @@ def vote(
     db: Session = Depends(database.get_db),
     current_user: int = Depends(oauth2.get_crruent_user),
 ):
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {vote.post_id} does not exist.",
+        )
+
     # Create the query
     vote_query = db.query(models.Vote).filter(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
